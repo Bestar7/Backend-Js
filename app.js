a78ef8ef3c71e189d64e9c67195a301240f8b087
Your Name <quentin.garwig@student.vinci.be>
2021-12-14 17:17:31 +0100

    salons backend

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var express = require("express");
 var logger = require("morgan");
 
 var questionRouter = require("./routes/questions");
-var userRouter = require("./routes/users")
+var userRouter = require("./routes/users");
+var salonRouter = require("./routes/salons");
+
 
 var app = express();
 
@@ -12,5 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/questions", questionRouter);
 app.use("/users", userRouter);
+app.use("/salons", salonRouter);
+
 
 module.exports = app;
