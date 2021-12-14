9518fe4973d315ce6fcbdd152ea6fa760471654c
Your Name <quentin.garwig@student.vinci.be>
2021-12-14 12:26:27 +0100

    users backend

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var logger = require("morgan");
 
 var questionRouter = require("./routes/questions");
+var userRouter = require("./routes/users")
 
 var app = express();
 
@@ -10,5 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/questions", questionRouter);
+app.use("/users", userRouter);
 
 module.exports = app;
